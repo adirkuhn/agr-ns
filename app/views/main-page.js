92721d34708060cb6e089c9f7b856793618c7ee3
Adir Kuhn <adirkuhn@gmail.com>
2015-10-05 20:02:17 -0300

    cadastro etapa1 layout

--- app/views/main-page.js
+++ app/views/main-page.js
@@ -1,6 +1,16 @@
 var vmModule = require("../view-model/main-view-model");
+var frameModule = require("ui/frame");
+
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = vmModule.mainViewModel;
 }
+
+function register() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/cadastro-etapa1");
+}
+
 exports.pageLoaded = pageLoaded;
+exports.register = register;
+
